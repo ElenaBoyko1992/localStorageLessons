57743ba983b3f188818925ce915ea83b7818326f
ElenaBoyko1992 <Bionka@inbox.ru>
2022-09-23 22:25:24 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState<number>(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }
+
+    const getFromLocalHandler = () => {
+        let newValue = localStorage.getItem('counterValue')
+        if (newValue) setValue(JSON.parse(newValue))
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeItemFromLocalStorageHandler = () => {
+        localStorage.removeItem('counterValue')
+    }
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+            <button onClick={getFromLocalHandler}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
